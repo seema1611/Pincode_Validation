a7761a6bcc604a8e5376f601efa35540affd5f56
seema1611 <seemarajpure16@gmail.com>
2020-04-11 00:58:00 +0530

    ADDED: Function to check pincode begin with alphabate or NOT

--- pincodeValidation.sh
+++ pincodeValidation.sh
@@ -3,18 +3,23 @@ shopt -s extglob
 
 #Pattern for pincode
 num_Pattern="^[0-9]{6}$"
+first_alphabate_Pattern="^[A-Za-z@$^*!#%&]{1,}[0-9]*$"
 
 #Function to check pattern for pincode
 function pincodeValidation() {
 
    if [[ $1 =~ $num_Pattern ]]
    then
-      echo "Valid Pincode Format"
+      echo "Valid, Proper pincode"
+   elif [[ $1 =~ $first_alphabate_Pattern ]]
+   then
+      echo "Invalid, Pincode start with alphabate or special character format"
    else
-      echo "Invalid Pincode Format"
+      echo "Invalid, Pincode has not in format "
    fi
 }
 
-read -p "Enter the pincode:" pincode
+read -p "Enter the pincode 6 digit:" pincode
 pincodeValidation $pincode
+
 
