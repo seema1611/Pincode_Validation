17fb173cc9f776482039d9f932b8265462cfa55e
seema1611 <seemarajpure16@gmail.com>
2020-04-11 19:21:41 +0530

    MODIFIED: Function to check pincode is proper or NOT

--- pincodeValidation.sh
+++ pincodeValidation.sh
@@ -2,9 +2,9 @@
 shopt -s extglob
 
 #Pattern for pincode
-num_Pattern="^([0-9]{3}[ ])*[0-9]{3}$"
-first_alphabate_Pattern="^[A-Za-z@$^*!#%&]{1,}[0-9]*$"
-last_alphabate_Pattern="^[0-9]*[A-Za-z]{1,}"
+num_Pattern="^[0-9]{3}[ ]?[0-9]{3}$"
+first_alphabate_Pattern="^[A-Za-z@$^*!#%&]{1,}[0-9]{1,}$"
+last_alphabate_Pattern="^[0-9]{1,}[A-Za-z]{1,}"
 
 #Function to check pattern for pincode
 function pincodeValidation() {
@@ -19,10 +19,9 @@ function pincodeValidation() {
    then
       echo "Invalid, Pincode Ends with alphabates format"
    else
-      echo "Invalid, Pincode has not in the format"
+      echo "Invalid, Pincode has not in the format or Empty"
    fi
 }
-
 read -p "Enter the pincode:" pincode
 pincodeValidation $pincode
 
